524d953526098f098b9234122ff90a30641e79b0
Felix Maurer <felix.m@online.de>
2014-05-08 19:18:20 +0200

    auch gut: Version

--- src/de/vp/Main.java
+++ src/de/vp/Main.java
@@ -4,7 +4,7 @@ public class Main {
     
     public static void main(String[] argv) {
         System.out.println("Test");
-        System.out.println("Hecker is d!!");
+        System.out.println("Hecker is da!!");
     }
     
 }

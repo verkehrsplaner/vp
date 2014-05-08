ab5d040f85aea998909c8cb9207b65d4d4ce5b0f
nicol_000 <nicol_000@BLACK-SHARK.Speedport_W723_V_Typ_A_1_01_001>
2014-05-08 22:47:48 +0200

    Test Nici

---
+++ src/de/vp/TestNici.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.vp;
+
+/**
+ *
+ * @author nicol_000
+ */
+public class TestNici {
+    //Test von mir.
+    
+}

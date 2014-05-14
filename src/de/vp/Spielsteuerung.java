2b2bbc7adcb06d8902a2105dc4caa43b72f6b05c
Felix Maurer <felix.m@online.de>
2014-05-14 22:38:13 +0200

    Mehr Spielsteuerung: - zugEinstellen() impl. - zugInsDepot() impl.

--- src/de/vp/Spielsteuerung.java
+++ src/de/vp/Spielsteuerung.java
@@ -5,25 +5,28 @@ package de.vp;
  * @author Felix
  */
 public class Spielsteuerung {
-    
-    private int depot, werkstatt, geld;
+
+    private int depot, werkstatt, geld, anzLinien;
     private boolean[][] hatBahnhof;
     private Stadtteil[][] teile;
     private Bahnhof[][] bahnhoefe;
-    
+    private Linie[] linien;
+
     // ========== Anfang Spielvariablen ==========
     private final int maxMinus = -10000000;
     private final int preisZug = 1000000;
     private final int geldZugZurueck = 50000;
     // ========== Ende Spielvariablen ==========
-    
+
     public Spielsteuerung(int hoehe, int breite) {
         depot = 0;
         werkstatt = 0;
+        anzLinien = 0;
         geld = 100000000; // 100 Mio.
         hatBahnhof = new boolean[hoehe][breite];
         teile = new Stadtteil[hoehe][breite];
         bahnhoefe = new Bahnhof[hoehe][breite];
+        linien = new Linie[20];
         for (int h = 0; h < hoehe; h++) {
             for (int b = 0; b < breite; b++) {
                 hatBahnhof[h][b] = false;
@@ -31,8 +34,11 @@ public class Spielsteuerung {
                 bahnhoefe[h][b] = null;
             }
         }
+        for (int i = 0; i < linien.length; i++) {
+            linien[i] = null;
+        }
     }
-    
+
     public boolean zugKaufen() {
         if (geld - preisZug >= maxMinus) {
             geld = geld - preisZug;
@@ -42,7 +48,7 @@ public class Spielsteuerung {
             return false;
         }
     }
-    
+
     public boolean zugVerschrotten() {
         if (depot > 0) {
             geld = geld + geldZugZurueck;
@@ -52,5 +58,25 @@ public class Spielsteuerung {
             return false;
         }
     }
-    
+
+    public boolean zugEinstellen(Linie l) {
+        if (depot > 0) {
+            l.zugEinstellen();
+            depot--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean zugInsDepot(Linie l) {
+        boolean b = l.zugEntfernen();
+        if (b) {
+            depot++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

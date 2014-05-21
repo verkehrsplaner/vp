6f6645775a0804889f5aa262202911b0d4853375
Maxi Krause <maxi.krause@me.com>
2014-05-21 22:51:13 +0200

    HäuserZeichnen 1.1

--- src/de/vp/SpielPanel.java
+++ src/de/vp/SpielPanel.java
@@ -61,6 +61,8 @@ public class SpielPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        setOpaque(false);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(

b76f668cd3d4711bdb639841282fe9e7082d0836
Christine Kaps <matsurikaxiii@yahoo.com>
2014-06-19 19:58:12 +0200

    Prolog Verbesserung

--- src/de/vp/PrologGUI.java
+++ src/de/vp/PrologGUI.java
@@ -5,6 +5,7 @@
  */
 package de.vp;
 
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.IOException;
@@ -23,21 +24,22 @@ public class PrologGUI extends javax.swing.JFrame {
     private int hoehe;
     private int breite;
 
+
     private String[] text = {
-        /*1*/"<html><p>Hallo und Herzlich Willkommen!</p><p> Ich freue mich sehr sie kennen zu lernen!</p></html>",
-        /*2*/ "<html><p>Mein Name ist Horst Bichler und ich bin Bürgermeister dieser bescheidenen Kommune...</p><p>Seit meiner Wahl bewegt sich hier einiges,und neue Industrie- und Wohngebiete entstehen.</p></html>",
-        /*3*/ "<html><p>Um dem immer dichteren Verkehrsaufkommen in der Stadt eine Alternative zu bieten, hat sich der Gemeinderat dazu entschlossen eine Verkehrsgesellschaft zu gründen und diese mit den Bau eines U-Bahnnetzes zu beauftragen!</p></html>",
-        /*4*/ "<html><p>Sie haben sich bei unserem Wettbewerb um die leitende Stelle in der Verkehrsgesellschaft durchgesetzt! Ich denke,damit haben wir die richtige Entscheidung getroffen! Ich denke wir werden gut miteinander zurecht kommen.</p></html>",
-        /*5*/ "<html><p>Die Anforderungen an Ihr Unternehmen sind einfach zu erklären: Ziel soll es sein, möglichst schnell alle Stadtteile miteinander zu vernetzen! Jeder unangebundene Stadtteil wir mit einer kleinen Strafe auf sich aufmerksam machen!</p></html>",
-        /*6*/ "<html><p>In der rechten Seitenleiste finden sie alles, um ihr Unternehmen zu leiten und auszubauen:</p></html>",
-        /*7*/ "<html><p>Einen neuen Bahnhof bauen sie, indem Sie hier drücken und danach auf den gewünschten Platz im Spielfeld klicken.</p></html>",
-        /*8*/ "<html><p>Eine neue Linie wird mit diesem Knopf erstellt!</p></html>",
-        /*9*/ "<html><p>Hier können Sie neue Züge für Ihre Linien kaufen bzw. verkaufen.</p><p>Diese Züge stehen dann in einem Depot bereit, sind jedoch noch keiner Linie zugewiesen.</html>",
-        /*10*/ "<html><p>Haben sie eine neue Linie erstellt können sie diese in der Seitenleiste aufrufen!</p><p>Hier können sie einen Bahnhof und so viele Züge wie sie haben der Linie zu weisen. Außerdem sind hier alle Informationen zur Linie gesammelt.</p></html>",
-        /*11*/ "<html><p>Klickt man auf einen Bahnhof, öffnet sich ebenfalls ein Fenster mit allen dort nötigen Informationen.</p></html>",
-        /*12*/ "<html><p>Die Zahl neben dem Bahnhof ist die Zahl der Personen die dort auf einen Zug warten. Ist die Zahl sehr hoch, sollten sie über eine bessere U-Bahn-Anbindung nachdenken;Ist die Zahl bei 0 ist alles im grünen Bereich.</p></html>",
-        /*13*/ "<html><p>Der News-Ticker am oberen Rand informiert Sie immer über alles was passiert. Ihn zu lesen schadet nicht.</p></html>",
-        /*14*/ "<html><p>Ich denke damit wäre alles gesagt. Stoßen wir auf eine erfolgreiche Zukunft an! Aber vergessen sie nicht ihre Pflichten!</p></html>"}; /*"<html><p></p></html>"*/
+        /*1*/"<html><center><p>Hallo und Herzlich Willkommen!</p><p> Ich freue mich sehr Sie kennen zu lernen!</p></center></html>",
+        /*2*/ "<html><center><p>Mein Name ist Horst Bichler und ich bin Bürgermeister dieser bescheidenen Kommune...</p><p>Seit meiner Wahl bewegt sich hier einiges,und neue Industrie- und Wohngebiete entstehen.</p></center></html>",
+        /*3*/ "<html><center><p>Um dem immer dichteren Verkehrsaufkommen in der Stadt eine Alternative zu bieten, hat sich der Gemeinderat dazu entschlossen eine Verkehrsgesellschaft zu gründen und diese mit den Bau eines U-Bahnnetzes zu beauftragen!</p></center></html>",
+        /*4*/ "<html><center><p>Sie haben sich bei unserem Wettbewerb um die leitende Stelle in der Verkehrsgesellschaft durchgesetzt! Ich denke,damit haben wir die richtige Entscheidung getroffen! Ich denke wir werden gut miteinander zurecht kommen.</p></center></html>",
+        /*5*/ "<html><center><p>Die Anforderungen an Ihr Unternehmen sind einfach zu erklären: Ziel soll es sein, möglichst schnell alle Stadtteile miteinander zu vernetzen! Jeder unangebundene Stadtteil wir mit einer kleinen Strafe auf sich aufmerksam machen!</p></center></html>",
+        /*6*/ "<html><center><p>In der rechten Seitenleiste finden sie alles, um ihr Unternehmen zu leiten und auszubauen:</p></center></html>",
+        /*7*/ "<html><center><p>Einen neuen Bahnhof bauen sie, indem Sie hier drücken und danach auf den gewünschten Platz im Spielfeld klicken.</p></center></html>",
+        /*8*/ "<html><center><p>Eine neue Linie wird mit diesem Knopf erstellt!</p></center></html>",
+        /*9*/ "<html><center><p>Hier können Sie neue Züge für Ihre Linien kaufen bzw. verkaufen.</p><p>Diese Züge stehen dann in einem Depot bereit, sind jedoch noch keiner Linie zugewiesen.</p></center></html>",
+        /*10*/ "<html><center><p>Haben sie eine neue Linie erstellt können sie diese in der Seitenleiste aufrufen!</p><p>Hier können sie einen Bahnhof und so viele Züge wie sie haben der Linie zu weisen. Außerdem sind hier alle Informationen zur Linie gesammelt.</p></center></html>",
+        /*11*/ "<html><center><p>Klickt man auf einen Bahnhof, öffnet sich ebenfalls ein Fenster mit allen dort nötigen Informationen.</p></center></html>",
+        /*12*/ "<html><center><p>Die Zahl neben dem Bahnhof ist die Zahl der Personen die dort auf einen Zug warten. Ist die Zahl sehr hoch, sollten sie über eine bessere U-Bahn-Anbindung nachdenken;Ist die Zahl bei 0 ist alles im grünen Bereich.</p></center></html>",
+        /*13*/ "<html><center><p>Der News-Ticker am oberen Rand informiert Sie immer über alles was passiert. Ihn zu lesen schadet nicht.</p></center></html>",
+        /*14*/ "<html><center><p>Ich denke damit wäre alles gesagt. Stoßen wir auf eine erfolgreiche Zukunft an! Aber vergessen sie nicht ihre Pflichten!</p></center></html>"}; /*"<html><p></p></html>"*/
 
     private Image[] bild;
     private int nummer;
@@ -53,7 +55,7 @@ public class PrologGUI extends javax.swing.JFrame {
                 /*2*/ ImageIO.read(this.getClass().getResource("images/mayor.png")),
                 /*3*/ ImageIO.read(this.getClass().getResource("images/mayor.png")),
                 /*4*/ ImageIO.read(this.getClass().getResource("images/mayor.png")),
-                /*5*/ ImageIO.read(this.getClass().getResource("images/mayor.png")),
+                /*5*/ ImageIO.read(this.getClass().getResource("images/linien.png")),
                 /*6*/ ImageIO.read(this.getClass().getResource("images/mayor.png")),
                 /*7*/ ImageIO.read(this.getClass().getResource("images/mayor.png")),
                 /*8*/ ImageIO.read(this.getClass().getResource("images/mayor.png")),
@@ -70,8 +72,11 @@ public class PrologGUI extends javax.swing.JFrame {
         breite = b;
         hoehe = h;
         nummer = 0;
+        Font myFont = new Font("Comic Sans MS", Font.BOLD, 15);
         initComponents();
         monolog.setText(text[nummer]);
+        monolog.setFont(myFont);
+        monolog.setHorizontalAlignment(monolog.CENTER);
 
         ImageIcon icon = new ImageIcon(getClass().getResource("images/icon.png"));
         setIconImage(icon.getImage());

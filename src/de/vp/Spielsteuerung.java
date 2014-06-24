09f63d599325931bd40413a42715e48379ade526
Nicolai Hackbart <nicolai.hackbart@faw-film.de>
2014-06-24 17:55:19 +0200

    speichern() in suuubbo!

--- src/de/vp/Spielsteuerung.java
+++ src/de/vp/Spielsteuerung.java
@@ -181,7 +181,7 @@ public class Spielsteuerung {
             while ((zeile = reader.readLine()) != null) {
                 // Einlesen
             }
-                reader.close();
+            reader.close();
         } catch (IOException ex) {
             System.err.println("Fehler beim Lesen der Datei!");
         }
@@ -207,43 +207,52 @@ public class Spielsteuerung {
             datei.println("depot:" + depot); // depot
             datei.println("w:" + werkstatt); // Werkstatt
             datei.println("date:" + strgTimer.getTime().getTime()); // UNIX x 1000
-            
+
             // Alle Stadtteile
             for (int h = 0; h < teile.length; h++) {
                 for (int b = 0; b < teile[h].length; b++) {
-                    if(teile[h][b] instanceof Haus) {
-                        datei.println("haus:" + h + b);
-                    }
-                    if(teile[h][b] instanceof Firma) {
-                        datei.println("firma:" + h + b);
-                    }
-                    if(teile[h][b] instanceof Park) {
-                        datei.println("park:" + h + b);
-                    }
-                    if(teile[h][b] instanceof Rathaus) {
-                        datei.println("firma:" + h + b);
+                    if (teile[h][b] != null) {
+                        if (teile[h][b] instanceof Haus) {
+                            datei.println("haus:" + h + "," + b);
+                        }
+                        if (teile[h][b] instanceof Firma) {
+                            datei.println("firma:" + h + "," + b);
+                        }
+                        if (teile[h][b] instanceof Park) {
+                            datei.println("park:" + h + "," + b);
+                        }
+                        if (teile[h][b] instanceof Rathaus) {
+                            datei.println("firma:" + h + "," + b);
+                        }
                     }
                 }
             }
-            
+
             // Alle Bahnhöfe
             for (int h = 0; h < teile.length; h++) {
                 for (int b = 0; b < teile[h].length; b++) {
-                    if(bahnhoefe[h][b] instanceof Bahnhof) {
-                        datei.println("bhf:" + h + b + bahnhoefe[h][b].getName() + bahnhoefe[h][b].getEinsteigen() + bahnhoefe[h][b].getAussteigen()); // Höhe, Breite, Name, Leute die einsteigen wollen, Leute die aussteigen wollen
+                    if (bahnhoefe[h][b] != null) {
+                        if (bahnhoefe[h][b] instanceof Bahnhof) {
+                            datei.println("bhf:" + h + "," + b + "," + bahnhoefe[h][b].getName() + "," + bahnhoefe[h][b].getEinsteigen() + "," + bahnhoefe[h][b].getAussteigen()); // Höhe, Breite, Name, Leute die einsteigen wollen, Leute die aussteigen wollen
+                        }
                     }
                 }
             }
-            
+
             // Alle Linien
             for (int i = 0; i < linien.length; i++) {
-                datei.println("linie:" + linien[i].getName() + linien[i].getFarbe().getRed() + linien[i].getFarbe().getGreen() + linien[i].getFarbe().getBlue() + linien[i].getZuege() + linien[i].getGruenesLicht()); // Name, RGB Farbe, Züge der Linie, boolean: Grünes Licht
-                // und derren Bahnhöfe
-                for(int bhf = 0; bhf < linien[i].getBahnhof().length; bhf++) {
-                    if(linien[bhf] != null){
-                    datei.println("bzl:" + linien[i].getBahnhof()[bhf].getY() + linien[i].getBahnhof()[bhf].getX()); // Bahnhofs Höhe und Breite
-                }}
-                datei.println("endeLinie");
+                if (linien[i] != null) {
+                    datei.println("linie:" + linien[i].getName() + "," + linien[i].getFarbe().getRed() + "," + linien[i].getFarbe().getGreen() + "," + linien[i].getFarbe().getBlue() + "," + linien[i].getZuege() + "," + linien[i].getGruenesLicht()); // Name, RGB Farbe, Züge der Linie, boolean: Grünes Licht
+
+                    // und derren Bahnhöfe
+                    Bahnhof[] bahnhof = linien[i].getBahnhof();
+                    for (int bhf = 0; bhf < bahnhof.length; bhf++) {
+                        if (bahnhof[bhf] != null) {
+                            datei.println("bzl:" + bahnhof[bhf].getY() + "," + bahnhof[bhf].getX()); // Bahnhofs Höhe und Breite
+                        }
+                    }
+                    datei.println("endeLinie");
+                }
             }
 
             datei.close();

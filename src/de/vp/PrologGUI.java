d019542677cd4e3ec96048080c25872904d38fae
Nicolai Hackbart <nicolai.hackbart@faw-film.de>
2014-06-25 20:49:29 +0200

    prolog kann neuen Text: LerrtaSTE UND SO

--- src/de/vp/PrologGUI.java
+++ src/de/vp/PrologGUI.java
@@ -76,6 +76,7 @@ public class PrologGUI extends javax.swing.JFrame {
         zurueck = new javax.swing.JButton();
         hintergrund = new BildPanel();
         vor = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Prolog");
@@ -113,6 +114,9 @@ public class PrologGUI extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Zum Überspringen Leertaste drücken!");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -123,7 +127,9 @@ public class PrologGUI extends javax.swing.JFrame {
                     .addComponent(hintergrund, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(zurueck)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
                         .addComponent(vor)))
                 .addContainerGap())
         );
@@ -135,7 +141,8 @@ public class PrologGUI extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(vor)
-                    .addComponent(zurueck))
+                    .addComponent(zurueck)
+                    .addComponent(jLabel1))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -172,6 +179,7 @@ public class PrologGUI extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel hintergrund;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JButton vor;
     private javax.swing.JButton zurueck;
     // End of variables declaration//GEN-END:variables
